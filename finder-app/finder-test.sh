faea4cac048698125e770f000d2d3bd5e2f9e194
nobody <nobody@abc.xyz>
2024-01-08 17:28:11 +0100

    [a4-p2] finder-test.sh: change config dir location to /etc/finder-app/config

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,8 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+CONFIGDIR=/etc/finder-app/conf
+username=$(cat ${CONFIGDIR}/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +33,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat ${CONFIGDIR}/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
